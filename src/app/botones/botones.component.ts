42dd3c27fa753351856e58fe4a5f1ea98d957205
Carlos Soto <3018554470101@ingenieria.usac.edu.gt>
2023-10-02 19:43:16 -0600

    Se realiza lo mismo pero con img

--- src/app/botones/botones.component.ts
+++ src/app/botones/botones.component.ts
@@ -11,12 +11,17 @@ export class BotonesComponent {
 
   ngOnInit(): void {}
 
-  text_color: string = "";
+  img : string = "https://images.pexels.com/photos/18399245/pexels-photo-18399245/free-photo-of-brunette-woman-posing-on-a-field.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
+  img2 : string = "https://images.pexels.com/photos/14395818/pexels-photo-14395818.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
+  img3 : string = "https://images.pexels.com/photos/15587503/pexels-photo-15587503/free-photo-of-wave.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
 
-  button_disabled:boolean = false;
-
-  activeDesactiveDisabled(){
-    this.button_disabled = !this.button_disabled;
+  SelectImg1(){
+    this.img = this.img2;
   }
+
+  SelectImg2(){
+    this.img = this.img3;
+  }
+
 
 }
